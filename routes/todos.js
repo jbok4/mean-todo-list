cfe2042d961ced48f1a82516b35f018e88bda76a
jbok4 <jbok4@aol.com>
2017-05-26 11:49:56 -0400

    setup mlab db and routes for json data

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://jackie:jackie@ds155201.mlab.com:55201/meantodos-jbok4', ['todos']);
 
+// Get all Todos
 router.get('/todos', function(req, res, next){
-  res.send('TODOS API');
+  db.todos.find(function(err, todos){
+    if(err){
+      res.send(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+  // Get single Todo
+router.get('/todo/:id', function(req, res, next){
+  db.todos.findOne({
+    _id: mongojs.ObjectId(req.params.id)
+  },function(err, todo){
+    if(err){
+      res.send(err);
+    } else {
+      res.json(todo);
+    }
+  });
+
 });
 
 module.exports = router;
