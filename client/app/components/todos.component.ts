5f8a4085649d4db991a23d2300231d033f806521
jbok4 <jbok4@aol.com>
2017-05-26 12:55:29 -0400

    ability to display saved to-do list items

---
+++ client/app/components/todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../Todo';
+
+@Component({
+  moduleId: module.id,
+  selector: 'todos',
+  templateUrl: 'todos.component.html'
+})
+
+export class TodosComponent implements OnInit { 
+  todos: Todo[];
+
+  constructor(private _todoService: TodoService){
+
+  }
+
+  ngOnInit(){
+    this.todos = [];
+    this._todoService.getTodos()
+      .subscribe(todos => {
+        this.todos = todos;
+      });
+  }
+}
